e6fc7baa17d8bdef76fb8b92a5ae4201a31da84a
AxelHartwig <axel@hartwig.nu>
2023-12-05 16:59:44 +0100

    hejhoppgalopp

--- dec5/Part2.java
+++ dec5/Part2.java
@@ -23,18 +23,17 @@ public class Part2 {
 
         public static void main(String[] args) {
             try {
-                Scanner scan = new Scanner(new File("input5.txt"));
+                Scanner scan = new Scanner(new File("test.txt"));
                 String[] seedStrings = scan.nextLine().split(" ");
-                HashSet<Range> rangeSet = new HashSet<>();
+                ArrayList<Range> rangeList = new ArrayList<>();
                 for (int i = 1; i < seedStrings.length; i += 2) {
                     long start = Long.parseLong(seedStrings[i]);
                     long range = Long.parseLong(seedStrings[i + 1]);
-                    rangeSet.add(new Range(start, range + start));
+                    rangeList.add(new Range(start, range + start-1));
                 }
 
-                System.out.println("added all ranges to map");
 
-                for (Range r : rangeSet) {
+                for (Range r : rangeList) {
                     System.out.println(r.toString());
                 }
 
@@ -49,35 +48,45 @@ public class Part2 {
                                 Long.parseLong(line.split(" ")[1]) + Long.parseLong(line.split(" ")[2]) - 1);
                         Range destinationRange = new Range(Long.parseLong(line.split(" ")[0]),
                                 Long.parseLong(line.split(" ")[0]) + Long.parseLong(line.split(" ")[2]) - 1);
-                        for (Range r : rangeSet) {
-                            if (r.contains(sourceRange)) {
-                                rangeSet.add(new Range(r.start, sourceRange.start - 1));
-                                rangeSet.add(destinationRange);
-                                rangeSet.add(new Range(sourceRange.end + 1, r.end));
-                                rangeSet.remove(r);
-                                break;
-                            } else if (!r.contains(sourceRange)) {
+                        System.out.println("sourcreange: " + sourceRange.toString());
+                        System.out.println("desrange: " + destinationRange.toString());
+                        for (int i = 0; i<rangeList.size(); i++) {
+                            if (rangeList.get(i).contains(sourceRange)) {
+                                System.out.println("contains");
+                                rangeList.add(new Range(rangeList.get(i).start, sourceRange.start - 1));
+                                rangeList.add(destinationRange);
+                                rangeList.add(new Range(sourceRange.end + 1, rangeList.get(i).end));
+                                rangeList.remove(i);
+                            } else if (!rangeList.get(i).contains(sourceRange)) {
+                                System.out.println("doesnt contain");
                                 // nothing
                             } else {
-                                if (r.start >= sourceRange.start) {
-                                    rangeSet.add(new Range(sourceRange.start, sourceRange.end));
-                                    rangeSet.add(new Range(sourceRange.end + 1, r.end));
-                                    rangeSet.remove(r);
-                                    break;
-                                } else if (sourceRange.end >= r.end) {
-                                    rangeSet.add(new Range(r.start, sourceRange.start - 1));
-                                    rangeSet.add(new Range(sourceRange.start, sourceRange.end));
-                                    rangeSet.remove(r);
-                                    break;
+                                System.out.println("else");
+                                if (rangeList.get(i).start >= sourceRange.start) {
+                                    rangeList.add(new Range(sourceRange.start, sourceRange.end));
+                                    rangeList.add(new Range(sourceRange.end + 1, rangeList.get(i).end));
+                                    rangeList.remove(rangeList.get(i));
+                                } else if (sourceRange.end >= rangeList.get(i).end) {
+                                    rangeList.add(new Range(rangeList.get(i).start, sourceRange.start - 1));
+                                    rangeList.add(new Range(sourceRange.start, sourceRange.end));
+                                    rangeList.remove(rangeList.get(i));
                                 } else {
+
+                                    System.out.println("missed all");
                                     continue;
                                 }
                             }
                         }
                     }
+                    for (Range r : rangeList) {
+                        System.out.println(r.toString());
+                    }
+                    System.out.println("");
+
+
                 }
                 long min = Long.MAX_VALUE;
-                for (Range r : rangeSet) {
+                for (Range r : rangeList) {
                     // System.out.println(r.toString());
                     if (r.start < min) {
                         min = r.start;
