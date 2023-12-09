729ad92c584762128d62b0b6a3e2b1d45919984c
AxelHartwig <axel@hartwig.nu>
2023-12-09 08:54:47 +0100

    dag9

---
+++ dec9/Part1.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Part1 {
+
+    public static void main(String[] args) {
+        try {
+            boolean part1 = false;
+            Scanner scan = new Scanner(new File("input9.txt"));
+            int sum = 0;
+            while (scan.hasNextLine()) {
+                boolean allZeroes = false;
+                int counter = 0;
+                ArrayList<ArrayList<Integer>> arrayListList = new ArrayList<>();
+                int[] line = Arrays.stream(scan.nextLine().split(" ")).mapToInt(c -> Integer.parseInt(c)).toArray();
+                arrayListList.add(new ArrayList<>());
+                for (int i = 0; i < line.length; i++) {
+                    arrayListList.get(0).add(line[i]);
+                }
+                while (!allZeroes) {
+                    arrayListList.add(new ArrayList<>());
+                    int current = arrayListList.get(counter).size() - 1;
+                    counter++;
+                    for (int i = 0; i < current; i++) {
+                        arrayListList.get(counter)
+                                .add(arrayListList.get(counter - 1).get(i + 1) - arrayListList.get(counter - 1).get(i));
+                    }
+                    for (int i = 0; i < arrayListList.get(counter).size(); i++) {
+                        allZeroes = true;
+                        if (arrayListList.get(counter).get(i) != 0) {
+                            allZeroes = false;
+                            break;
+                        }
+                    }
+                }
+
+                if(part1) {
+                    arrayListList.get(arrayListList.size() - 1).add(0);
+                    for (int i = arrayListList.size() - 2; i >= 0; i--) {
+                        arrayListList.get(i).add(arrayListList.get(i).get(arrayListList.get(i).size() - 1) + arrayListList.get(i + 1).get(arrayListList.get(i + 1).size() - 1));
+                    } 
+                    sum += arrayListList.get(0).get(arrayListList.get(0).size()-1);
+                }
+
+                if(!part1) {
+                    arrayListList.get(arrayListList.size() - 1).add(0,0);
+                    for(int i = arrayListList.size() - 2; i>=0; i--) {
+                        arrayListList.get(i).add(0, arrayListList.get(i).get(0) - arrayListList.get(i+1).get(0));
+                    }
+                    sum += arrayListList.get(0).get(0);
+                }
+
+                
+            }
+            System.out.println(sum);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
