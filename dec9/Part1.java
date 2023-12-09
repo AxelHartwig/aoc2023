7a40073eaf85ae7ea4ef3bcb3aa89f54472d06bf
AxelHartwig <axel@hartwig.nu>
2023-12-09 09:52:48 +0100

    dag 9

--- dec9/Part1.java
+++ dec9/Part1.java
@@ -16,9 +16,19 @@ public class Part1 {
                 ArrayList<ArrayList<Integer>> arrayListList = new ArrayList<>();
                 int[] line = Arrays.stream(scan.nextLine().split(" ")).mapToInt(c -> Integer.parseInt(c)).toArray();
                 arrayListList.add(new ArrayList<>());
-                for (int i = 0; i < line.length; i++) {
-                    arrayListList.get(0).add(line[i]);
+
+                if (part1) {
+                    for (int i = 0; i < line.length; i++) {
+                        arrayListList.get(0).add(line[i]);
+                    }
                 }
+
+                if (!part1) {
+                    for (int i = line.length - 1; i >= 0; i--) {
+                        arrayListList.get(0).add(line[i]);
+                    }
+                }
+
                 while (!allZeroes) {
                     arrayListList.add(new ArrayList<>());
                     int current = arrayListList.get(counter).size() - 1;
@@ -36,23 +46,13 @@ public class Part1 {
                     }
                 }
 
-                if(part1) {
-                    arrayListList.get(arrayListList.size() - 1).add(0);
-                    for (int i = arrayListList.size() - 2; i >= 0; i--) {
-                        arrayListList.get(i).add(arrayListList.get(i).get(arrayListList.get(i).size() - 1) + arrayListList.get(i + 1).get(arrayListList.get(i + 1).size() - 1));
-                    } 
-                    sum += arrayListList.get(0).get(arrayListList.get(0).size()-1);
+                arrayListList.get(arrayListList.size() - 1).add(0);
+                for (int i = arrayListList.size() - 2; i >= 0; i--) {
+                    arrayListList.get(i).add(arrayListList.get(i).get(arrayListList.get(i).size() - 1)
+                            + arrayListList.get(i + 1).get(arrayListList.get(i + 1).size() - 1));
                 }
+                sum += arrayListList.get(0).get(arrayListList.get(0).size() - 1);
 
-                if(!part1) {
-                    arrayListList.get(arrayListList.size() - 1).add(0,0);
-                    for(int i = arrayListList.size() - 2; i>=0; i--) {
-                        arrayListList.get(i).add(0, arrayListList.get(i).get(0) - arrayListList.get(i+1).get(0));
-                    }
-                    sum += arrayListList.get(0).get(0);
-                }
-
-                
             }
             System.out.println(sum);
         } catch (Exception e) {
