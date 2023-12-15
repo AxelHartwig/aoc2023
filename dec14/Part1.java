b18052f1bedd9d8d478b10179e548ffe01256b0b
AxelHartwig <axel@hartwig.nu>
2023-12-15 12:04:24 +0100

    boom

--- dec14/Part1.java
+++ dec14/Part1.java
@@ -8,43 +8,21 @@ public class Part1 {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("test.txt"));
-        ArrayList<String> cols = new ArrayList<>();
-        ArrayList<String> tiltedCols = new ArrayList<>();
-        String line = scan.nextLine();
-        for(int i = 0; i<line.length(); i++) {
-        cols.add(Character.toString(line.charAt(i)));
-        }
-        
+        Scanner scan = new Scanner(new File("input14.txt"));
+        ArrayList<String> in = new ArrayList<>();        
         while(scan.hasNextLine()) {
-            line = scan.nextLine();
-            for(int i = 0; i<line.length(); i++) {
-                cols.set(i, cols.get(i).concat(Character.toString(line.charAt(i))));
-            }
+            in.add(scan.nextLine());
         }
+        part1(in);
+    }
 
-        for(int k = 0; k<cols.size(); k++) {
-            tiltedCols.add("");
-            String[] split = cols.get(k).split("#");
-            int c = 0;
-            while(cols.get(k).charAt(c) == '#') {
-                tiltedCols.set(k, tiltedCols.get(k).concat("#"));
-                System.out.println("hej");
-                c++;
-            }
-            for(int i = 0; i<split.length; i++) {
-                char[] sorted = split[i].toCharArray();
-                Arrays.sort(sorted);
-                for(int j = sorted.length-1; j >= 0; j--) {
-                    tiltedCols.set(k, tiltedCols.get(k).concat(Character.toString(sorted[j])));
-                }
-                tiltedCols.set(k, tiltedCols.get(k).concat("#"));
-            }
-            while(tiltedCols.get(k).length() < cols.get(k).length()) {
-                tiltedCols.set(k, tiltedCols.get(k).concat("#"));
-            }
-        }
-          System.out.println(tiltedCols);
+    private static void part1(ArrayList<String> in) {
+        ArrayList<String> cols;
+        ArrayList<String> tiltedCols;
+        cols = turn(in);
+        tiltedCols = tilt(cols);
+        
+    
         int part1 = 0;
         for(int i = 0; i<tiltedCols.size(); i++) {
             for(int j = 0; j<tiltedCols.get(0).length(); j++) {
@@ -55,5 +33,55 @@ public class Part1 {
             
         }
         System.out.println("part1 = " + part1);
+    }
+
+    private static void part2(ArrayList<String> in) {
+        int cycleLength = 0;
+        ArrayList<String> temp = in;
+        while(true) {
+            in = turn(in);
+            in = tilt(in);
+            for(int i = 0; i<temp.size(); i++) {
+            }
+        }
+    }
+
+    private static ArrayList<String> tilt(ArrayList<String> rocks) {
+        ArrayList<String> tilted = new ArrayList<>();
+        for(int k = 0; k<rocks.size(); k++) {
+            tilted.add("");
+            String[] split = rocks.get(k).split("#");
+
+  
+            for(int i = 0; i<split.length; i++) {
+                char[] sorted = split[i].toCharArray();
+                Arrays.sort(sorted);
+                for(int j = sorted.length-1; j >= 0; j--) {
+                    tilted.set(k, tilted.get(k).concat(Character.toString(sorted[j])));
+                }
+                tilted.set(k, tilted.get(k).concat("#"));
+            }
+            while(tilted.get(k).length() < rocks.get(k).length()) {
+                tilted.set(k, tilted.get(k).concat("#"));
+            }
+
+            if(tilted.get(k).length() > rocks.get(k).length()) {
+                tilted.set(k,tilted.get(k).substring(0,tilted.get(k).length()-1));
+            }
+        }
+        return tilted;
+    }
+
+    private static ArrayList<String> turn(ArrayList<String> rocks) {
+        ArrayList<String> turned = new ArrayList<>();
+        for(int i = 0; i<rocks.size(); i++) {
+            turned.add("");
+        }
+        for(String s : rocks) {
+            for(int i = 0; i<s.length(); i++) {
+                turned.set(i, turned.get(i).concat(Character.toString(s.charAt(i))));
+            }
+        }
+        return turned;
     }
 }
