3d24c308f6d450ae5297c28281bb49cd776966e7
AxelHartwig <axel@hartwig.nu>
2023-12-15 15:28:54 +0100

    13

---
+++ dec13/Part1.java
@@ -0,0 +1,121 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Part1 {
+    
+    static boolean equalsWithSmudge(String s1, String s2) {
+        boolean one = true;
+        for(int i = 0; i<s1.length(); i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                if(one) {
+                    one = false;
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("Input13.txt"));
+        ArrayList<String> pattern = new ArrayList<>();
+        ArrayList<String> transPattern = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+        boolean row = false;
+        boolean col = false;
+        boolean flag = true;
+        while(scan.hasNextLine()) {
+            row = false;
+            col = false;
+            String line = scan.nextLine();
+            if(line.isBlank()) {
+                //checks row
+                
+                for(int i = 0; i<pattern.size()-1; i++) {
+                    if(pattern.get(i).equals(pattern.get(i+1)) || equalsWithSmudge(pattern.get(i), pattern.get(i+1))) {
+                        if(!pattern.get(i).equals(pattern.get(i+1))) {
+                            flag = false;
+                        } else {
+                            flag = true;
+                        }
+                        int j = i;
+                        int k = i+1;
+                        row = true;
+                        while(j >= 0 && k < pattern.size()-1 && row) {
+                            row &= pattern.get(j).equals(pattern.get(k));
+                            if(!pattern.get(j).equals(pattern.get(k)) && flag) {
+                                if(equalsWithSmudge(pattern.get(j), pattern.get(k))) {
+                                    flag = false;
+                                }
+                            }                            
+                            j--;
+                            k++;
+                            }
+                        if(row){
+                            sum += (i+1)*100;
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+  
+                if(!row) {
+
+                    //bygger transponerad matris
+                    for(int i = 0; i<pattern.get(0).length(); i++) {
+                        sb = new StringBuilder();
+                        for(int j = 0; j<pattern.size(); j++) {
+                            sb.append(pattern.get(j).charAt(i));
+                        }
+                        System.out.println(sb.toString());
+                        transPattern.add(sb.toString());
+                    }
+                    System.out.println("");
+  
+
+                    for(int i = 0; i<transPattern.size()-1; i++) {
+                        if(transPattern.get(i).equals(transPattern.get(i+1)) || equalsWithSmudge(transPattern.get(i), transPattern.get(i+1))) {
+                            if(!transPattern.get(i).equals(transPattern.get(i+1))) {
+                                flag = false;
+                            } else {
+                                flag = true;
+                            }
+                            int j = i;
+                            int k = i+1;
+                            col = true;
+                            while(j >= 0 && k < transPattern.size()-1 && col) {
+                                col &= transPattern.get(j).equals(transPattern.get(k));
+                                if(!transPattern.get(j).equals(transPattern.get(k)) && flag) {
+                                    if(equalsWithSmudge(transPattern.get(j), transPattern.get(k))) {
+                                        flag = false;
+                                    }
+                                }                            
+                                j--;
+                                k++;
+                            }
+
+                            if(col){
+                                sum += (i+1);
+                                break;
+                            } else {
+                                continue;
+                            }
+                    }
+                }
+                }
+                pattern.clear();
+                transPattern.clear();
+            } else {
+                pattern.add(line);
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
